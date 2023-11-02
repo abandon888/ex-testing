20d7d3b8a34810e4137b13b0d7673b7196718e6e
abandon888 <1876746719@qq.com>
2023-11-02 18:19:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const area = document.querySelector('#地区')
+const setGrade = document.querySelector('#设置等级')
+const aTags = document.querySelectorAll('#设置等级 a')
+const paths = document.querySelectorAll('#地区 path')
+const scoreText = document.querySelector('#分数')
+//基本逻辑是：点击地区时获取地区信息，弹出设置等级div，点击设置等级，将数据存储到localStorage中，然后更新数据
+//获取点击的地区
+area.addEventListener('click', (event) => {
+
+})
+//获取设置的等级及其id
+aTags.forEach(
+
+)
+//更新地图数据
+const applyData = () => {
+
+}
